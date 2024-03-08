98fe93d2d8fb35e2fcf5e62f0380db56c61fe75a
Pedro Galembeck <51977156+galembeck@users.noreply.github.com>
2024-03-08 12:59:46 -0300

    feat: add first class challenge

---
+++ ClassChallenge.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number, biggest_number;
+    int counter = 0;
+
+    while (counter != 10) {
+        printf("\n Insert a number: ");
+        scanf("%d", &number);
+
+        if (counter == 1) {
+            biggest_number = number;
+        } else if (number > biggest_number) {
+            biggest_number = number;
+        }
+
+        counter = counter + 1;
+    }
+
+    printf("\n\n The biggest number, among the inserted numbers, is: %d.", biggest_number);
+
+    system("pause");
+    return 0;
+}
