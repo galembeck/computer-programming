fcdd869070d991e663ef768dd80293fbdaa03f0b
Pedro Galembeck <51977156+galembeck@users.noreply.github.com>
2024-03-12 08:32:39 -0300

    Update ClassChallenge02.c

--- ClassChallenge02.c
+++ ClassChallenge02.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Desafio de sala 02:
+    Leia quantos números inteiros forem necessários ate que a soma seja maior ou igual a 100; imprima o valor da soma e a quantidade de números lidos.
+*/
+
 int main() {
     float number, sum = 0;
     int quantity = 0;
