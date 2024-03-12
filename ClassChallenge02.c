f745690a3dc07d12708fea24fb2e3cb201fd1524
Pedro Galembeck <51977156+galembeck@users.noreply.github.com>
2024-03-12 08:30:45 -0300

    Update ClassChallenge02.c

--- ClassChallenge02.c
+++ ClassChallenge02.c
@@ -2,3 +2,19 @@
 #include <stdlib.h>
 
 int main() {
+    float number, sum = 0;
+    int quantity = 0;
+    
+    while (sum <= 100) {
+        printf("\n Insert a number: ");
+        scanf("%d", &number);
+        
+        sum = sum + number;
+        quantity++;
+    }
+    
+    printf("\n Sum = %d, quantity = %d.", sum, quantity);
+    
+    system("pause")
+    return 0;
+}
