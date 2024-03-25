83088871b566075f78f270bf3c8e9ad9b71423f8
Pedro Galembeck <51977156+galembeck@users.noreply.github.com>
2024-03-25 12:46:53 -0300

    Update SwitchMenu.c

--- SwitchMenu.c
+++ SwitchMenu.c
@@ -7,8 +7,11 @@ void mainMessage() {
 
     printf("\n Escolha uma das opções abaixo para executar:");
     printf("\n  Opção 1: Entrada de valores numéricos");
-    printf("\n  Opção 2: Sair");
+    printf("\n  Opção 2: Soma dos valores inseridos");
+    printf("\n  Opção 3: Sair");
 }
+
+void exit (int status);
 
 int main() {
     setlocale(LC_ALL, "portuguese");
@@ -21,18 +24,24 @@ int main() {
         printf("\n\n Opção: ");
         scanf("%d", &option);
 
-        float first_value, second_value;
+        float first_value, second_value, sum;
 
         switch(option) {
             case 1:
                 system("cls");
+
                 printf("\n Insira o primeiro valor: ");
                 scanf("%f", &first_value);
                 printf("\n Insira o segundo valor: ");
                 scanf("%f", &second_value);
-
-                mainMessage();
                 break;
+            case 2:
+                system("cls");
+
+                sum = first_value + second_value;
+                printf("\n Soma dos valores inseridos: %.2f.", sum);
+            case 3:
+                exit(0);
             default:
                 printf("\n Opção inválida! Escolha uma das opções disponíveis acima.\n\n");
         }
