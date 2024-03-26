6f60a543ec24661beabfcaf5df0a749fb54d5ac7
Pedro Galembeck <51977156+galembeck@users.noreply.github.com>
2024-03-26 14:23:08 -0300

    Update Exercise01.c

--- Aula 04 - Prática/Exercise01/Exercise01.c
+++ Aula 04 - Prática/Exercise01/Exercise01.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <locale.h>
 
-/* Exercício 01:
+/* ExercÃ­cio 01:
     Escrever um algoritmo que leia uma quantidade desconhecida de
-    números e conte quantos deles estão nos seguintes intervalos:
+    nÃºmeros e conte quantos deles estÃ£o nos seguintes intervalos:
     [0.25], [26,50], [51,75] e [76,100].
 
-    A entrada de dados deve terminar quando for lido um número negativo.
+    A entrada de dados deve terminar quando for lido um nÃºmero negativo.
 */
 
 int main() {
@@ -16,29 +16,35 @@ int main() {
     int numbers_quantity, number, i;
     int first_intrvl = 0, second_intrvl = 0, third_intrvl = 0, fourth_intrvl = 0;
 
-    printf("\n Informe a quantidade de números a serem lidos: ");
+    printf("\n Informe a quantidade de nÃºmeros a serem lidos: ");
     scanf("%d", &numbers_quantity);
 
     for(i = 0; i < numbers_quantity; i++) {
-        printf("\n Digite um número: ");
+        printf("\n Digite um nÃºmero: ");
         scanf("%d", &number);
 
         if (number <= 0) {
-            printf("\n A operação foi finalizada, pois o número digitado é negativo.\n\n");
+            printf("\n A operaÃ§Ã£o foi finalizada, pois o nÃºmero digitado Ã© negativo.\n\n");
         }
 
-        if((number >= 0) && (number <= 25)) {
+        if(number <= 25) {
             first_intrvl++;
-        } else if ((number >= 26) && (number <= 50)) {
-            second_intrvl++;
-        } else if ((number >= 51) && (number <= 75)) {
-            third_intrvl++;
-        } else if ((number >= 76) && (number <= 100)) {
-            fourth_intrvl++;
+        } else {
+            if(number <= 50) {
+                second_intrvl++;
+            } else {
+                if(number <= 75) {
+                    third_intrvl++;
+                } else {
+                    if(number <= 100) {
+                        fourth_intrvl++;
+                    }
+                }
+            }
         }
     }
 
-    printf("\n\n [0, 25]: %d números\n\n [26, 50]: %d números\n\n [51, 75]: %d números\n\n [76, 100]: %d números.\n\n", first_intrvl, second_intrvl, third_intrvl, fourth_intrvl);
+    printf("\n\n [0, 25]: %d nÃºmeros\n\n [26, 50]: %d nÃºmeros\n\n [51, 75]: %d nÃºmeros\n\n [76, 100]: %d nÃºmeros.\n\n", first_intrvl, second_intrvl, third_intrvl, fourth_intrvl);
 
     system("pause");
     return 0;
